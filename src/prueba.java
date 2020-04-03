8cc9a16f6ebc952dec461dabad2e08a1eca754da
GastonVidart <gaston.vidart@gmail.com>
2020-04-03 20:11:31 -0300

    prueba 1.2

--- src/prueba.java
+++ src/prueba.java
@@ -9,6 +9,7 @@ public class prueba {
     public static void main(String[] args) {
         System.out.println("Hola desde Holanda");
         System.out.println("pedrito fue a la guerra");        
-        System.out.println("pedrito fue a la guerra1231");    
+        System.out.println("pedrito fue a la guerra1231");      
+        System.out.println("Pedrito no volvio que decis");
     }
 }
