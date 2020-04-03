a91d63a64cc96f0b8f1c3c87b10d85aa5cd690aa
guido <guido@192.168.0.175>
2020-04-03 20:23:06 -0300

    prueba 2.1

--- src/prueba.java
+++ src/prueba.java
@@ -5,11 +5,13 @@
  */
 public class prueba {
     //hola Gaston
-    
+
     public static void main(String[] args) {
         System.out.println("Hola desde Holanda");
-        System.out.println("pedrito fue a la guerra");        
-        System.out.println("pedrito fue a la guerra1231");      
+        System.out.println("pedrito fue a la guerra");
+        System.out.println("pedrito fue a la guerra1231");
         System.out.println("Pedrito no volvio que decis");
+        
+        System.out.println("Guido hizo un cambio");
     }
 }
