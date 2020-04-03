3276324ba502acfecfd4ce1f8581b7174a5d495c
GastonVidart <gaston.vidart@gmail.com>
2020-04-03 20:23:17 -0300

    prueba 2.2

--- src/prueba.java
+++ src/prueba.java
@@ -11,5 +11,7 @@ public class prueba {
         System.out.println("pedrito fue a la guerra");        
         System.out.println("pedrito fue a la guerra1231");      
         System.out.println("Pedrito no volvio que decis");
+        
+        System.out.println("Iron Man sigue vivo, todos lo sabemos");
     }
 }
