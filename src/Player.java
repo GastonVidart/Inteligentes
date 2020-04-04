455f6caf3fe481d10c7d5cf47a12bb62fc5a1b7a
guido <guido@192.168.0.175>
2020-04-04 18:47:41 -0300

    Let'sGO

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,41 @@
 
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
 // @author guido
 public class Player {
 
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop
+        while (true) {
+            int myShipCount = in.nextInt(); // the number of remaining ships
+            int entityCount = in.nextInt(); // the number of entities (e.g. ships, mines or cannonballs)
+
+            for (int i = 0; i < entityCount; i++) {
+                int entityId = in.nextInt();
+                String entityType = in.next();
+                int x = in.nextInt();
+                int y = in.nextInt();
+                int arg1 = in.nextInt();
+                int arg2 = in.nextInt();
+                int arg3 = in.nextInt();
+                int arg4 = in.nextInt();
+
+            }
+
+            for (int i = 0; i < myShipCount; i++) {
+                // Write an action using System.out.println()
+                // To debug: System.err.println("Debug messages...");
+                System.out.println("WAIT"); // Any valid action, such as "WAIT" or "MOVE x y"
+
+            }
+        }
+    }
+
+    public static void printMensaje(String msj) {
+        System.err.println(msj);
+    }
 }
